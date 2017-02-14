68e8e7773aaca5e75df389daa3f84ff914ac75e6
millarba <millarba@gmail.com>
2017-02-13 21:24:29 -0600

    added authorization

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170210012403) do
+ActiveRecord::Schema.define(version: 20170214024835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20170210012403) do
     t.string   "style"
     t.string   "ibu"
     t.string   "alcohol"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "api_key"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
